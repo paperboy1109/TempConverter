c953d2c22c5b3666366e7cfcd311fd4b1ffd42cc
Daniel J Janiak <danieljjaniak@icloud.com>
2017-01-04 14:13:21 -0800

    Initial Commit

---
+++ TempConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TempConverter
+//
+//  Created by Daniel J Janiak on 1/4/17.
+//  Copyright © 2017 redwoodempiredev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
