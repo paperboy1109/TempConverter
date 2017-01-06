dffa9471989dc82e92eeb44b4623dd6f5311f576
Daniel J Janiak <danieljjaniak@icloud.com>
2017-01-05 20:34:47 -0800

    feat: Add a background that has a customizable color gradient

--- TempConverter/ViewController.swift
+++ TempConverter/ViewController.swift
@@ -10,19 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    var gradientLayer: CAGradientLayer!
+    
     // MARK: - Outlets
-
+    
     @IBOutlet var textField: UITextField!
     
     @IBOutlet var label: UILabel!
+    
+    @IBOutlet var conversionCountLabel: UILabel!
+    
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //self.view.backgroundColor = UIColor(red: 255/255, green: 85/255, blue: 61/255, alpha: 0.8)
+        
+        let numConversionsObject = UserDefaults.standard.object(forKey: "")
+        
+        if let numConversions = numConversionsObject as? Double {
+            conversionCountLabel.text = String(numConversions)
+        }
+        
+        print("numConversionsObject is: \(numConversionsObject)")
+        
+        if numConversionsObject == nil {
+            conversionCountLabel.text = "0"
+        }
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        createGradientLayer()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -45,9 +71,25 @@ class ViewController: UIViewController {
             
         }
         
+        //UserDefaults.standard.set(<#T##value: Double##Double#>, forKey: "conversionCounter")
+        
     }
     
-
-
+    // MARK: - Helpers
+    
+    func createGradientLayer() {
+        
+        gradientLayer = CAGradientLayer()
+        
+        gradientLayer.frame = self.view.bounds
+        
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor]
+        
+        self.view.layer.addSublayer(gradientLayer)
+    }
+    
+    
+    
+    
 }
 
